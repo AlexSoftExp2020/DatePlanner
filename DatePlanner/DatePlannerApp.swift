f1c7b7d0dfd0544d47e327ae4c4179d0fafe58a8
Alex <oleksii.pidhornyi@gmail.com>
2024-12-17 14:33:05 +0100

    Initial Commit

---
+++ DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Oleksii on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
