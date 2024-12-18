2fefd5a4ea6ca3f706d09edd2d9d5d85563f095b
Alex <oleksii.pidhornyi@gmail.com>
2024-12-18 21:49:15 +0100

    add sfsymbolstyling and taskrow

---
+++ DatePlanner/SFSymbolStyling.swift
@@ -0,0 +1,22 @@
+//
+//  SFSymbolStyling.swift
+//  DatePlanner
+//
+//  Created by Oleksii on 18.12.2024.
+//
+
+import SwiftUI
+
+struct SFSymbolStyling: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .imageScale(.large)
+            .symbolRenderingMode(.monochrome)
+    }
+}
+
+extension View {
+    func sfSymbolStyling() -> some View {
+        modifier(SFSymbolStyling())
+    }
+}
